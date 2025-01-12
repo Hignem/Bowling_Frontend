ac086567e912bc6045366383ec0d03f3891b7838
Hignem <81192237+Hignem@users.noreply.github.com>
2025-01-13 00:14:00 +0100

    Deleted debug watermark
    Usunięto znak wodny "debug" aplikacji

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class BowlingReservationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Bowling Reservation App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
